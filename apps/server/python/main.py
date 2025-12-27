22f527900c1e76be2b124ed84c6f55b2ddf3ead2
xerosf <sansith.fernando@gmail.com>
2025-12-28 01:54:14 +0530

    Gem

--- apps/server/python/main.py
+++ apps/server/python/main.py
@@ -1,9 +1,12 @@
-from transformers import AutoModel, AutoTokenizer
+from transformers import AutoModel, AutoTokenizer, AutoProcessor
 
 model = AutoModel.from_pretrained("jinaai/jina-clip-v2", trust_remote_code=True)
 tokenizer = AutoTokenizer.from_pretrained("jinaai/jina-clip-v2", trust_remote_code=True)
 
 model.save_pretrained("./jina-v2-local")
 tokenizer.save_pretrained("./jina-v2-local")
+
+processor = AutoProcessor.from_pretrained("jinaai/jina-clip-v2", trust_remote_code=True)
+processor.save_pretrained("./jina-v2-local")
 
 print("Download complete.")
